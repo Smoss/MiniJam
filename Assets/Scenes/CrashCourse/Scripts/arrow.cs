ae3268860a3cf50f574b6f4b0cae0773bf888a9b
Courtney Toder <ctoder@broadinstitute.org>
2015-09-08 14:16:03 -0400

    adding in message functionality

---
+++ Assets/Scenes/CrashCourse/Scripts/arrow.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class arrow : MonoBehaviour {
+	public Sprite none;
+	public Sprite up;
+	public Sprite left;
+	public Sprite right;
+	public Sprite down;
+	bool state;
+	// Use this for initialization
+	void Start () {
+		if (Random.Range (-1.0f, 1.0f) > 0) {
+			this.GetComponent<SpriteRenderer> ().sprite = on;
+			state = true;
+		} else {
+			this.GetComponent<SpriteRenderer> ().sprite = off;
+			state = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("Vertical")>0) {
+			state = !state;
+			if(state)
+			{
+				this.GetComponent<SpriteRenderer> ().sprite = on;
+			}	
+			else{
+				this.GetComponent<SpriteRenderer> ().sprite = off;
+			}
+			
+		}
+		else if(Input.GetAxis ("Vertical")>0){
+		}
+		else{
+		}
+	}
+}
