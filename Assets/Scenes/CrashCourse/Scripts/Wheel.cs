5fd7f5aed8948fc52e314dfb3cf1ea62f3cbc52c
Smoss <Smoss117@gmail.com>
2015-09-08 12:34:33 -0400

    Added crashCourse Stuff

---
+++ Assets/Scenes/CrashCourse/Scripts/Wheel.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wheel : MonoBehaviour {
+	
+	public float speed = 1.0f;
+	public string axisName = "Horizontal";
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetButton("Button 1")) {
+			// left
+			transform.Rotate(0.0f, 0.0f, 5.0f);  // does nothing, just a bad guess
+		}
+		
+		if(Input.GetButton("Button 2")) {
+			// right
+			transform.Rotate(0.0f, 0.0f, -5.0f);  // does nothing, just a bad guess
+		}
+		
+	}
+}
