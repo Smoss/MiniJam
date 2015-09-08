1c393ac32fa3bdf7be86e7a8102622dcb70fb355
smoss <Smoss117@gmail.com>
2015-09-08 14:01:53 -0400

    Added Switches

--- Assets/Scenes/CrashCourse/Scripts/Ship.cs
+++ Assets/Scenes/CrashCourse/Scripts/Ship.cs
@@ -2,10 +2,8 @@
 using System.Collections;
 
 public class Ship : MonoBehaviour {
-	Vector2 position;
 	// Use this for initialization
 	void Start () {
-		position = new Vector2 (0.0f, Random.Range (-2.0f, 2.0f));
 	
 	}
 	
---
+++ Assets/Scenes/CrashCourse/Scripts/Switch1.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switch1 : MonoBehaviour {
+	public Sprite on;
+	public Sprite off;
+	bool state;
+	bool swapped;
+	// Use this for initialization
+	void Start () {
+		if (Random.Range (-1.0f, 1.0f) > 0) {
+			this.GetComponent<SpriteRenderer> ().sprite = on;
+			state = true;
+		} else {
+			this.GetComponent<SpriteRenderer> ().sprite = off;
+			state = false;
+		}
+		swapped = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("Vertical")>0 && !swapped) {
+			state = !state;
+			swapped = true;
+			if(state)
+			{
+				this.GetComponent<SpriteRenderer> ().sprite = on;
+			}	
+			else{
+				this.GetComponent<SpriteRenderer> ().sprite = off;
+			}
+
+		}
+		else if(Input.GetAxis ("Vertical")>0 && swapped){
+		}
+		else{
+			swapped = false;
+		}
+	}
+}
